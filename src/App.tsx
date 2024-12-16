73a36b99d95bfd7a66d5e2cbbae13a63e55d883b
hicass <hicass.dev@gmail.com>
2024-12-16 13:44:15 -0800

    Add logic to the main page of the app to log users in and out

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { signInWithGoogle, signOutUser } from './utils/authUtils';
+import { auth } from './firebase/firebaseConfig';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
 
-  return <h1>Hello World!</h1>;
-}
+  // Listener for Authentication State
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // Update the user state with the current authenticated user
+      setUser(currentUser);
+    });
+
+    // Unsubscribe from the listener when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <>
+      {user ? (
+        <>
+          <p>Hello, {user.displayName}</p>{' '}
+          <button onClick={signOutUser}>Sign Out</button>
+        </>
+      ) : (
+        <>
+          <h1>Hello World!</h1>
+          <button onClick={signInWithGoogle}>Sign In With Google</button>
+        </>
+      )}
+    </>
+  );
+};
 
 export default App;
