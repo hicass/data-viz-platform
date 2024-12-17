45903a0325db87e3b422b4fa4c49b4ce9a525503
hicass <hicass.dev@gmail.com>
2024-12-17 12:05:10 -0800

    Add 'Variables' button to the Peromance Card Section of the dashboard view

--- src/components/Dashboard/PerformanceCardSection/PerformanceCardSection.tsx
+++ src/components/Dashboard/PerformanceCardSection/PerformanceCardSection.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import PerformanceCard, { PerformanceCardType } from './PerformanceCard';
+import Button from '../../ui/Button';
 
 interface PerformanceCardsProps {
   performanceCards: PerformanceCardType[];
@@ -11,11 +12,18 @@ const PerformanceCardSection: FC<PerformanceCardsProps> = ({
 }) => {
   return (
     <section className="flex flex-col h-full w-full lg:min-w-40 lg:w-2/5">
-      <h2 className="text-2xl font-semibold">Key Performance Indicators</h2>
+      <div className="w-full flex items-center justify-between">
+        <h2 className="text-2xl font-semibold">Key Performance Indicators</h2>
+        <Button
+          text="Variables"
+          style="text-sm bg-darkGrey hover:bg-white/5"
+          onClick={() => console.log('Button Clicked!')}
+        />
+      </div>
 
       <div className="h-full grid grid-cols-2 gap-6 mt-4">
-      {/* Map through the performanceCards array and render a Card component for each item */}
-      {performanceCards.map((card, idx) => (
+        {/* Map through the performanceCards array and render a Card component for each item */}
+        {performanceCards.map((card, idx) => (
           <PerformanceCard {...card} key={idx} />
         ))}
       </div>
