50aaab1e00e828e1fca4adaaca47d4fd3120a607
hicass <hicass.dev@gmail.com>
2024-12-16 17:26:31 -0800

    Add comments to PerformanceCardSection components for better clarity

--- src/components/Dashboard/PerformanceCardSection/PerformanceCardSection.tsx
+++ src/components/Dashboard/PerformanceCardSection/PerformanceCardSection.tsx
@@ -5,12 +5,16 @@ interface PerformanceCardsProps {
   performanceCards: CardType[];
 }
 
-const PerformanceCards: FC<PerformanceCardsProps> = ({ performanceCards }) => {
+// Component to render Performance Card related elements
+const PerformanceCardSection: FC<PerformanceCardsProps> = ({
+  performanceCards,
+}) => {
   return (
     <section>
-      <h2 className='text-2xl font-semibold'>Key Performance Indicators</h2>
+      <h2 className="text-2xl font-semibold">Key Performance Indicators</h2>
 
       <div className="h-full grid grid-cols-2 gap-6 mt-4">
+        {/* Map through the performanceCards array and render a Card component for each item */}
         {performanceCards.map((card, idx) => (
           <Card {...card} key={idx} />
         ))}
@@ -19,4 +23,4 @@ const PerformanceCards: FC<PerformanceCardsProps> = ({ performanceCards }) => {
   );
 };
 
-export default PerformanceCards;
+export default PerformanceCardSection;
