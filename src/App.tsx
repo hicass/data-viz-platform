86de03b4612220563c263e563eb9b95c08b2209d
hicass <hicass.dev@gmail.com>
2024-12-18 12:03:28 -0800

    Remove unneeded 'bg-black' rule from the App's wrapper div

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <div className="bg-black h-screen w-full flex text-white">
+      <div className="h-screen w-full flex text-white overflow-hidden">
         <Nav user={user} />
         <Routes>
           <Route path="/" element={user ? <Dashboard /> : <Auth />} />
